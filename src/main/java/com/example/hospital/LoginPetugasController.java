cc5c616ea054ef5c6047a6e8ca112656757dd117
arkan <maulanahelmy7@gmail.com>
2022-12-11 05:22:52 +0700

    added alert for login screen

--- src/main/java/com/example/hospital/LoginPetugasController.java
+++ src/main/java/com/example/hospital/LoginPetugasController.java
@@ -1,5 +1,6 @@
 package com.example.hospital;
 
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
@@ -10,7 +11,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
@@ -23,10 +26,91 @@ public class LoginPetugasController implements Initializable {
 
     @FXML
     private TextField usernameField;
+    Boolean apakah_alert_active = false;
+    Boolean apakah_alertbelumpenuh_active = false;
+    @FXML
+    private HBox alerta;
+    @FXML
+    private HBox alertbelumpenuh;
+
+
+
+    public void closeAlerta() {
+        TranslateTransition moveOut = new TranslateTransition();
+        moveOut.setNode(alerta);
+        moveOut.setDuration(Duration.millis(200));
+        moveOut.setCycleCount(1);
+        moveOut.setByY(-94);
+        moveOut.play();
+        apakah_alert_active = false;
+    }
+
+    public void openAlerta() {
+        TranslateTransition moveIn = new TranslateTransition();
+        moveIn.setNode(alerta);
+        moveIn.setDuration(Duration.millis(200));
+        moveIn.setCycleCount(1);
+        moveIn.setByY(94);
+        moveIn.play();
+        apakah_alert_active = true;
+//        moveIn.setOnFinished(e -> {
+//            Timeline timeline = new Timeline(new KeyFrame(
+//                    Duration.seconds(7),
+//                    event -> closeAlerta()
+//            ));
+//            timeline.play();
+//        });
+    }
+
+    public void closeAlertBelumPenuh() {
+        TranslateTransition moveOut = new TranslateTransition();
+        moveOut.setNode(alertbelumpenuh);
+        moveOut.setDuration(Duration.millis(200));
+        moveOut.setCycleCount(1);
+        moveOut.setByY(-94);
+        moveOut.play();
+        apakah_alertbelumpenuh_active = false;
+    }
+
+    public void openAlertBelumPenuh() {
+        TranslateTransition moveIn = new TranslateTransition();
+        moveIn.setNode(alertbelumpenuh);
+        moveIn.setDuration(Duration.millis(200));
+        moveIn.setCycleCount(1);
+        moveIn.setByY(94);
+        moveIn.play();
+        apakah_alertbelumpenuh_active = true;
+//        moveIn.setOnFinished(e -> {
+//            Timeline timeline = new Timeline(new KeyFrame(
+//                    Duration.seconds(7),
+//                    event -> closeAlerta()
+//            ));
+//            timeline.play();
+//        });
+    }
 
     public void loginAction(ActionEvent actionEvent) throws IOException {
         if (usernameField.getText().equals("") || passwordField.getText().equals("")) {
             System.out.println("Monggo diisi");
+            if(apakah_alert_active){
+                closeAlerta();
+            }
+            if (apakah_alertbelumpenuh_active) {
+
+                TranslateTransition moveOut = new TranslateTransition();
+                moveOut.setNode(alertbelumpenuh);
+                moveOut.setDuration(Duration.millis(200));
+                moveOut.setCycleCount(1);
+                moveOut.setByY(-94);
+                moveOut.play();
+                apakah_alertbelumpenuh_active = false;
+                moveOut.setOnFinished(e -> {
+                    openAlertBelumPenuh();
+                });
+            } else {
+                openAlertBelumPenuh();
+            }
+
         } else if (usernameField.getText().equals("admin")&& passwordField.getText().equals("admin")) {
             FXMLLoader Loader = new FXMLLoader(getClass().getResource("HomepagePetugas.fxml"));
             Parent Page = Loader.load();
@@ -36,7 +120,24 @@ public class LoginPetugasController implements Initializable {
             primaryStage.setScene(Scene);
         } else{
             System.out.println("username password salah");
+            if(apakah_alertbelumpenuh_active){
+                closeAlertBelumPenuh();
+            }
+            if (apakah_alert_active) {
 
+                TranslateTransition moveOut = new TranslateTransition();
+                moveOut.setNode(alerta);
+                moveOut.setDuration(Duration.millis(200));
+                moveOut.setCycleCount(1);
+                moveOut.setByY(-94);
+                moveOut.play();
+                apakah_alert_active = false;
+                moveOut.setOnFinished(e -> {
+                    openAlerta();
+                });
+            } else {
+                openAlerta();
+            }
         }
     }
 
